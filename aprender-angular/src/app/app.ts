293ccc4b6390e0cc3c9e27e2eb6acd06e578de29
carlitosxm <carlos_xavi@live.com>
2026-05-03 18:27:04 -0500

    reto 4
    reto 4

--- aprender-angular/src/app/app.ts
+++ aprender-angular/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Producto } from './producto/producto';
+import { miPerfil } from './mi-perfil/miPerfil';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Producto ,miPerfil],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ aprender-angular/src/app/mi-perfil/miPerfil.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"mi-perfil",
+    templateUrl:"miPerfil.html",
+    styleUrl:"miPerfil.css"
+})
+
+export class miPerfil{
+
+}
