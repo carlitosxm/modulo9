e72d1150fde9a03b57a7b1e74502db3ca7364543
carlitosxm <carlos_xavi@live.com>
2026-05-04 19:24:55 -0500

    Reto 6
    Reto 6

--- aprender-angular/src/app/mi-perfil/miPerfil.ts
+++ aprender-angular/src/app/mi-perfil/miPerfil.ts
@@ -1,14 +1,26 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:"mi-perfil",
-    templateUrl:"miPerfil.html",
-    styleUrl:"miPerfil.css"
+    selector: "mi-perfil",
+    templateUrl: "miPerfil.html",
+    styleUrl: "miPerfil.css"
 })
 
-export class miPerfil{
- constructor(){
-    alert("¡Bienvenido al perfil, estás dentro de la aplicación!");
-    console.log("Se cargó el componente de perfil correctamente.");
- }
+export class miPerfil {
+
+    public nombre: string;
+    public edad:number;
+    public ocupacion: string;
+    public direccion: string;
+    public telefono: string;
+
+    constructor() {
+        this.nombre=" Juan Pérez.";
+        this.edad=30;
+        this.ocupacion="Soy Desarrollador de Software.";
+        this.direccion="Av. Amazonas y Río Coca, Quito, Ecuador.";
+        this.telefono="+593 999 999 999";
+
+        console.log("Se cargó el componente de perfil correctamente.");
+    }
 }
--- aprender-angular/src/app/producto/producto.ts
+++ aprender-angular/src/app/producto/producto.ts
@@ -2,13 +2,23 @@ import { Component } from "@angular/core";
 
 
 @Component({
-    selector:"mi-producto",
-    templateUrl:"producto.html",
-    styleUrl:"producto.css"
+    selector: "mi-producto",
+    templateUrl: "producto.html",
+    styleUrl: "producto.css"
 })
 
-export class Producto{
- constructor(){
+export class Producto {
 
- }
+    public nombre: string;
+    public marca: string;
+    public precio: number;
+    public descripcion: string;
+
+
+    constructor() {
+        this.nombre = "PORTATIL";
+        this.marca = "ASUS";
+        this.precio = 475;
+        this.descripcion="mi producto es un ordenador";
+    }
 }
