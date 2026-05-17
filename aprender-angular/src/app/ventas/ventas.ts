a6b928b64f363a04d5ef415ffdd7670f88f7b766
carlitosxm <carlos_xavi@live.com>
2026-05-17 13:41:42 -0500

    Reto 17
    Reto 17

--- aprender-angular/src/app/ventas/ventas.ts
+++ aprender-angular/src/app/ventas/ventas.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { Pelicula } from '../pelicula/pelicula';
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-ventas',
-  imports: [Pelicula, NgClass],
+  imports: [Pelicula, NgClass, NgStyle],
   templateUrl: './ventas.html',
   styleUrl: './ventas.css',
 })
 export class Ventas {
   public colorear:boolean = false;
 
-  public mostrar:string = "si";
+  public mostrar:string = "i";
+
+  misEstilos={
+    'background-color': 'yellow',
+    'padding': '10px',
+    'font-weight': 'bold',
+    'font-style': 'italic'
+  }
 }
