942cc585284fbe3553cc98d2333c121876dd25dc
carlitosxm <carlos_xavi@live.com>
2026-05-17 13:24:08 -0500

    Reto 16
    Reto 16

--- aprender-angular/src/app/app.ts
+++ aprender-angular/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Producto } from './producto/producto';
 import { miPerfil } from './mi-perfil/miPerfil';
 import { Usuario } from './usuario/usuario';
+import { Ventas } from './ventas/ventas';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Producto ,miPerfil, Usuario],
+  imports: [RouterOutlet, Producto ,miPerfil, Usuario, Ventas],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- aprender-angular/src/app/ventas/ventas.ts
+++ aprender-angular/src/app/ventas/ventas.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { Pelicula } from '../pelicula/pelicula';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-ventas',
-  imports: [],
+  imports: [Pelicula, NgClass],
   templateUrl: './ventas.html',
   styleUrl: './ventas.css',
 })
-export class Ventas {}
+export class Ventas {
+  public colorear:boolean = false;
+
+  public mostrar:string = "si";
+}
